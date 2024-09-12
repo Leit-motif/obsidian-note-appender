9e650c10e5f4fc18a4d43bdc5719881fcc93b56e
Amrit Chana <Random96503@gmail.com>
2024-09-12 18:17:17 -0500

    first iteration, file picker for input and output, monthly auto-generation

--- main.ts
+++ main.ts
@@ -1,85 +1,46 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	inputFolder: string;
+	outputFolder: string;
+	autoGenerate: boolean;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	inputFolder: '5.0 Journal/5.1 Daily',
+	outputFolder: '5.0 Journal/5.3 Monthly',
+	autoGenerate: false
 }
 
-export default class MyPlugin extends Plugin {
+export default class MonthlyRecapPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		// Add a ribbon icon
+		this.addRibbonIcon('calendar', 'Generate Monthly Recap', (evt: MouseEvent) => {
+			this.generateMonthlyRecap();
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// Add a command
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'generate-monthly-recap',
+			name: 'Generate Monthly Recap',
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				this.generateMonthlyRecap();
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		// Add settings tab
+		this.addSettingTab(new MonthlyRecapSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
+		// Add auto-generation check
+		this.registerInterval(
+			window.setInterval(() => this.checkAutoGenerate(), 3600000) // Check every hour
+		);
 	}
 
 	async loadSettings() {
@@ -89,28 +50,86 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async generateMonthlyRecap() {
+		const vault = this.app.vault;
+		const dailyNotesFolder = this.settings.inputFolder;
+
+		// Get all files in the daily notes folder
+		const dailyNotes = vault.getFiles().filter(file => 
+			file.path.startsWith(dailyNotesFolder) && file.extension === 'md'
+		);
+
+		// Group files by year and month
+		const groupedNotes = this.groupNotesByYearAndMonth(dailyNotes);
+
+		for (const [yearMonth, files] of Object.entries(groupedNotes)) {
+			const [year, month] = yearMonth.split('-');
+			const outputFileName = `${year}-${month}.md`;
+			const outputFilePath = `${this.settings.outputFolder}/${outputFileName}`;
+
+			let recapContent = '';
+			for (const file of files) {
+				const content = await vault.read(file);
+				const cleanedContent = this.cleanFileContent(content);
+				recapContent += cleanedContent + '\n\n';
+			}
+
+			// Create or update the recap file
+			const existingFile = vault.getAbstractFileByPath(outputFilePath);
+			if (existingFile instanceof TFile) {
+				await vault.modify(existingFile, recapContent);
+			} else {
+				await vault.create(outputFilePath, recapContent);
+			}
+		}
+
+		new Notice('Monthly recap generated successfully!');
+	}
+
+	groupNotesByYearAndMonth(files: TFile[]): Record<string, TFile[]> {
+		const grouped: Record<string, TFile[]> = {};
+		for (const file of files) {
+			const match = file.name.match(/^(\d{4})-(\d{2})-\d{2}/);
+			if (match) {
+				const [, year, month] = match;
+				const key = `${year}-${month}`;
+				if (!grouped[key]) {
+					grouped[key] = [];
+				}
+				grouped[key].push(file);
+			}
+		}
+		return grouped;
+	}
+
+	cleanFileContent(content: string): string {
+		// Remove Dailies block
+		content = content.replace(/# Dailies\s*\n(?:- \[[ x]\] .*\n?)*/g, '');
+
+		// Remove calendar-nav block
+		content = content.replace(/---\s*\n```calendar-nav\n```/g, '');
+
+		// Remove Daily Law block
+		content = content.replace(/# Daily Law\s*\n(?:- \[[ x]\] .*\n?)*/g, '');
+
+		return content;
+	}
+
+	checkAutoGenerate() {
+		if (this.settings.autoGenerate) {
+			const now = new Date();
+			if (now.getDate() === 1 && now.getHours() === 0) {
+				this.generateMonthlyRecap();
+			}
+		}
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class MonthlyRecapSettingTab extends PluginSettingTab {
+	plugin: MonthlyRecapPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: MonthlyRecapPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,13 +140,34 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Input Folder')
+			.setDesc('Folder path for daily notes')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Example: folder1/folder2')
+				.setValue(this.plugin.settings.inputFolder)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.inputFolder = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Output Folder')
+			.setDesc('Folder path for monthly recap files')
+			.addText(text => text
+				.setPlaceholder('Example: folder1/folder2')
+				.setValue(this.plugin.settings.outputFolder)
+				.onChange(async (value) => {
+					this.plugin.settings.outputFolder = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Auto Generate')
+			.setDesc('Automatically generate recap for the previous month on the first day of each month')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.autoGenerate)
+				.onChange(async (value) => {
+					this.plugin.settings.autoGenerate = value;
 					await this.plugin.saveSettings();
 				}));
 	}
